8a3d81a9147c80860a2074a01d962b8d66c14c47
etech <email@gmail.com>
2019-01-22 15:39:28 +0530

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const { ApolloServer } = require('apollo-server');
+
+
+
+import { typeDefs, resolvers } from "./schema"
+
+// var app = express();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    mocks: true,
+    mockEntireSchema: false,
+});
+
+server.listen(4000);
